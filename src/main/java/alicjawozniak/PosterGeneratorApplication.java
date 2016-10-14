7955beddc40a3360f200da3f02cf51eb35196cdb
Alicja Woźniak <alicja.wozniak@silksh.pl>
2016-10-14 11:20:35 +0200

    init

---
+++ src/main/java/alicjawozniak/PosterGeneratorApplication.java
@@ -0,0 +1,12 @@
+package alicjawozniak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosterGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosterGeneratorApplication.class, args);
+    }
+}
---
+++ src/test/java/alicjawozniak/PosterGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package alicjawozniak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PosterGeneratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
